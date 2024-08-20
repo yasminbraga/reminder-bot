6198f87f737ccb71ca8bd28742d3ba8f82cd4357
yasminbraga <yasminbragateixeira@gmail.com>
2024-08-20 16:44:56 -0300

    add dotenv

--- index.js
+++ index.js
@@ -1,69 +1,128 @@
 const { Client, GatewayIntentBits, Events, Collection } = require("discord.js");
-const { token } = require("./config.json");
 const fs = require("node:fs");
 const path = require("node:path");
 const cron = require("node-cron");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.commands = new Collection();
-const foldersPath = path.join(__dirname, "commands");
-const commandFolders = fs.readdirSync(foldersPath);
+// client.commands = new Collection();
+// const foldersPath = path.join(__dirname, "commands");
+// const commandFolders = fs.readdirSync(foldersPath);
 
-for (const folder of commandFolders) {
-  const commandsPath = path.join(foldersPath, folder);
-  const commandFiles = fs
-    .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".js"));
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
+// for (const folder of commandFolders) {
+//   const commandsPath = path.join(foldersPath, folder);
+//   const commandFiles = fs
+//     .readdirSync(commandsPath)
+//     .filter((file) => file.endsWith(".js"));
+//   for (const file of commandFiles) {
+//     const filePath = path.join(commandsPath, file);
+//     const command = require(filePath);
 
-    if ("data" in command && "execute" in command) {
-      client.commands.set(command.data.name, command);
-    } else {
-      console.log(
-        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
-      );
-    }
-  }
-}
+//     if ("data" in command && "execute" in command) {
+//       client.commands.set(command.data.name, command);
+//     } else {
+//       console.log(
+//         `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+//       );
+//     }
+//   }
+// }
 
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
   eachMinuteMsg.start();
 });
 
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
+// client.on(Events.InteractionCreate, async (interaction) => {
+//   if (!interaction.isChatInputCommand()) return;
 
-  const command = interaction.client.commands.get(interaction.commandName);
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found`);
-    return;
-  }
+//   const command = interaction.client.commands.get(interaction.commandName);
+//   if (!command) {
+//     console.error(`No command matching ${interaction.commandName} was found`);
+//     return;
+//   }
 
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
-    } else {
-      await interaction.reply({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
-    }
-  }
-});
+//   try {
+//     await command.execute(interaction);
+//   } catch (error) {
+//     console.error(error);
+//     if (interaction.replied || interaction.deferred) {
+//       await interaction.followUp({
+//         content: "There was an error while executing this command!",
+//         ephemeral: true,
+//       });
+//     } else {
+//       await interaction.reply({
+//         content: "There was an error while executing this command!",
+//         ephemeral: true,
+//       });
+//     }
+//   }
+// });
 
-client.login(token);
+client.login(process.env.DISCORD_TOKEN);
 
-const eachMinuteMsg = cron.schedule("0 18 * * 2,4,6", () => {
+const eachMinuteMsg = cron.schedule("* * * * *", () => {
+  // "0 18 * * 2,4,6"
   const channel = client.channels.cache.get("920676429052661793");
   channel.send("Tirar o lixo!");
 });
+
+// const { REST, Routes } = require("discord.js");
+// const fs = require("node:fs");
+// const path = require("node:path");
+
+// const commands = [];
+// // Grab all the command folders from the commands directory you created earlier
+// const foldersPath = path.join(__dirname, "commands");
+// const commandFolders = fs.readdirSync(foldersPath);
+
+// for (const folder of commandFolders) {
+//   // Grab all the command files from the commands directory you created earlier
+//   const commandsPath = path.join(foldersPath, folder);
+//   const commandFiles = fs
+//     .readdirSync(commandsPath)
+//     .filter((file) => file.endsWith(".js"));
+//   // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+//   for (const file of commandFiles) {
+//     const filePath = path.join(commandsPath, file);
+//     const command = require(filePath);
+//     if ("data" in command && "execute" in command) {
+//       commands.push(command.data.toJSON());
+//     } else {
+//       console.log(
+//         `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+//       );
+//     }
+//   }
+// }
+
+// // Construct and prepare an instance of the REST module
+// const rest = new REST().setToken(process.env.DISCORD_TOKEN);
+
+// // and deploy your commands!
+// (async () => {
+//   try {
+//     console.log(
+//       `Started refreshing ${commands.length} application (/) commands.`
+//     );
+
+//     // The put method is used to fully refresh all commands in the guild with the current set
+//     const data = await rest.put(
+//       Routes.applicationGuildCommands(
+//         process.env.CLIENT_ID,
+//         process.env.GUILD_ID
+//       ),
+//       { body: commands }
+//     );
+
+//     console.log(
+//       `Successfully reloaded ${data.length} application (/) commands.`
+//     );
+//   } catch (error) {
+//     // And of course, make sure you catch and log any errors!
+//     console.error(error);
+//   }
+// })();
