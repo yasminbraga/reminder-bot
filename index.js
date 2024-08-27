5d1362649faf14f687d7691d936611d45d8ed20a
yasminbraga <yasminbragateixeira@gmail.com>
2024-08-27 15:01:59 -0300

    teste

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
   trashReminder.start();
+  eachMinuteMsg.start();
 });
 
 client.login(process.env.DISCORD_TOKEN);
 
-// const eachMinuteMsg = cron.schedule("* * * * *", () => {
-//   const channel = client.channels.cache.get("920676429052661793");
-//   channel.send("Todo minuto");
-// });
+const eachMinuteMsg = cron.schedule("* * * * *", () => {
+  const channel = client.channels.cache.get("920676429052661793");
+  channel.send("Todo minuto");
+});
 
 const trashReminder = cron.schedule(
   "0 18 * * 2,4,6",
